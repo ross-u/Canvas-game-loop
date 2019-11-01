e7c094f14dfcb28d4db7fa470477cb60abae40f1
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-11-01 16:05:29 +0100

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -14,8 +14,6 @@ Game.prototype.start = function() {
   this.canvas = document.querySelector('canvas');
   this.ctx = this.canvas.getContext('2d');
 
-  console.log(this.canvas);
-
   // Set the canvas to be same as the viewport size
   this.containerWidth = this.canvasContainer.offsetWidth;
   this.containerHeight = this.canvasContainer.offsetHeight;
